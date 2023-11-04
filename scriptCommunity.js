5ba8e7973487f6005461eadfd8a0fb43ac416e71
renkodiranje <renata.tukic@gmail.com>
2023-11-05 00:13:54 +0100

    section height

--- scriptCommunity.js
+++ scriptCommunity.js
@@ -192,8 +192,8 @@ function displayCurrentUserProfile() {
         let joined = join.toLocaleDateString();
         delBtn.style.position = "relative";
         delBtn.style.bottom = 0;
-        delBtn.style.width = "50%";
-        delBtn.style.margin = "0 25%";
+        delBtn.style.width = "25%";
+
         delBtn.textContent = "Delete acount";
         let sectionContent = `<div class="card" id="card">
       <div class="row g-0">
